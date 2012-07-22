0adb5cc8f1f57750f6526474a682303ead6ef570
Ray Bunnage <rayb@seattlespineinstitute.com>
2012-07-21 20:56:50 -0700

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoardMaker::Application.config.secret_token = 'eaab1a5f1e691c18a8f52c6ec682ddb96a9a47556d5dd6c6c96f0df276025bf2dbe7aed8219ad3f2f378089cbb45d0a6014cf4acf73266474613649d3241d3bb'
